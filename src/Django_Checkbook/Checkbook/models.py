dc756968232af62f63cb4b3562109c3c6d23d841
Jason <AarsZach@yahoo.com>
2021-08-09 21:04:58 -0600

    Part 3 fix

--- src/Django_Checkbook/Checkbook/models.py
+++ src/Django_Checkbook/Checkbook/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    # First Name on the account
+    first_name = models.CharField(max_length=50)
+    # Last Name on the account
+    last_name = models.CharField(max_length=50)
+    # Starting Balance of the Class in hours
+    starting_balance = models.DecimalField(max_digits=15, decimal_places=2)
+    # Add the models manager
+    Accounts = models.Manager()
+
+    # Returns a more comprehensive name
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+TransactionTypes = {
+    ('deposit', 'deposit'),
+    ('withdrawal', 'withdrawal'),
+}
+
+class Transaction(models.Model):
+    # Date of Transaction
+    date = models.DateField()
+    # Type of Transaction [Deposit or Withdrawal]
+    type = models.CharField(max_length=10, choices=TransactionTypes)
+    # Amount
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    # Description
+    description = models.CharField(max_length=100)
+    # Account it applies to [this is a Foreign Key]
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    # Add the models manager
+    Transactions = models.Manager()
+
+    # Returns a more comprehensive name
+    def __str__(self):
+        return '{} | {} | {}'.format(self.account, self.type, self.date)
--- src/Django_Checkbook/Checkbook/urls.py
+++ src/Django_Checkbook/Checkbook/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.urls.resolvers import URLPattern
 from . import views
 
-urlpattern = [
+urlpatterns = [
     path('', views.home, name='index'),
     path('create/', views.create_account, name='create'),
     path('balance/', views.balance, name='balance'),
-    path('transaction/', views.balance, name='transaction')
+    path('transaction/', views.transaction, name='transaction'),
 ]
